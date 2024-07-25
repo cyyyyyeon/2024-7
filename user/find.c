2333a33a40ad4c0cc34e20373f86a29e00448433
cyyyyyeon <1449235503@qq.com>
2024-07-25 09:25:26 +0800

    修改lab1

--- user/find.c
+++ user/find.c
@@ -11,11 +11,11 @@ char* fmtname(char *path)
   for(p=path+strlen(path); p >= path && *p != '/'; p--)
     ;
   p++;
-  memmove(buf, p, strlen(p)+1);
+  memmove(buf, p, strlen(p)+1);//复制末尾空字符
   return buf;
 }
 void find(char *path,char *filename){
-    char buf[512], *p;
+  char buf[512], *p;
   int fd;
   struct dirent de;
   struct stat st;
@@ -44,11 +44,11 @@ void find(char *path,char *filename){
     *p++ = '/';
     while(read(fd, &de, sizeof(de)) == sizeof(de)){
       // 跳过无效目录项（inum 为 0 或 1，或者是当前目录"."或父目录".."）
-    if(de.inum == 0 ||strcmp(de.name, ".")==0|| strcmp(de.name, "..")==0)
+      if(de.inum == 0 ||strcmp(de.name, ".")==0|| strcmp(de.name, "..")==0)
         continue;
-    memmove(p, de.name, strlen(de.name));
-    p[strlen(de.name)] = 0;
-	find(buf, filename);    
+      memmove(p, de.name, strlen(de.name));
+      p[strlen(de.name)] = 0;
+	    find(buf, filename);    
     }
     break;
   }
@@ -56,11 +56,11 @@ void find(char *path,char *filename){
 }
 
 int main(int argc,char *argv[]){
-    //检查参数
-    if(argc!=3){
-        fprintf(2,"usage:find <path> <filename>\n");
-        exit(1);
-    }
-    find(argv[1],argv[2]);
-    exit(0);
+  //检查参数
+  if(argc!=3){
+    fprintf(2,"usage:find <path> <filename>\n");
+    exit(1);
+  }
+  find(argv[1],argv[2]);
+  exit(0);
 }
--- user/sleep.c
+++ user/sleep.c
@@ -3,15 +3,15 @@
 #include "user/user.h"
 
 int main(int argc, char *argv[]) {
-	  if (argc != 2) {
-		      fprintf(2, "usage: sleep ticks\n");
-		          exit(1);
-			    }
-	    int ticks = atoi(argv[1]);	//转换成数字
-	      if (ticks < 0) {
-		          fprintf(2, "sleep: ticks must be non-negative\n");
-			      exit(1);
-			        }
-	        sleep(ticks);
-		  exit(0);
+	if (argc != 2) {
+		fprintf(2, "usage: sleep ticks\n");
+		exit(1);
+	}
+	int ticks = atoi(argv[1]);	//转换成数字
+	if (ticks < 0) {
+		fprintf(2, "sleep: ticks must be non-negative\n");
+		exit(1);
+	}
+	sleep(ticks);
+	exit(0);
 }
